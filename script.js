efe060596507f9aa2abc5592548686365ac7a7be
Carlos Cerqueira <90733399+carlosvcerqueira@users.noreply.github.com>
2022-07-12 23:43:51 -0300

    alterações (#1)

---
+++ script.js
@@ -0,0 +1,30 @@
+const numeros = document.querySelectorAll('input')
+const telaResultado = document.querySelector('#result')
+const buttonDarkMode = document.querySelector('.mode')
+let conta;
+
+function botao(value) {
+    conta = telaResultado.value += value
+}
+
+function limpar() {
+    telaResultado.value = ""
+}
+
+function calc() {
+    const resultado = eval(conta)
+    telaResultado.value = resultado
+}
+
+function retirar() {
+    const palavraCompleta = telaResultado.value
+    const ultimaLetra = palavraCompleta.substring(0, telaResultado.value.lenght - 1)
+    console.log(ultimaLetra)
+
+    telaResultado.value = ultimaLetra
+}
+
+buttonDarkMode.addEventListener('click', function() {
+    document.documentElement.classList.toggle('ativo')
+    buttonDarkMode.classList.toggle('ativo')
+})
